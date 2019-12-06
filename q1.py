58e40550e6a07fc4d8a870600e0aaabcf71dad49
awphi <jellymoos3@gmail.com>
2019-12-06 14:23:58 +0000

    Fix q1

--- q1.py
+++ q1.py
@@ -1,31 +1,33 @@
-table_quartic = ['-'] * 23;
-table_double = ['-'] * 23;
-
 def h(k):
 	return (4 * k + 7) % 23
 
 def h2(k):
-	return (17 - (k % 17))
+	return 17 - (k % 17)
 
 def hash_double(keys):
+	table_double = ['-'] * 23;
 	for k in keys:
-		hk = h(k)
+		ak = hk = h(k)
+		h2k = h2(k)
+		j = 0
 
-		if(table_double[hk] != '-'):
-			hk = h2(k)
+		while(table_double[ak] != '-'):
+			ak = (hk + (j * h2k)) % 23
+			j += 1
 
-		table_double[hk] = 'X'
+		table_double[ak] = k
 	return table_double
 
 
 def hash_quartic(keys):
+	table_quartic = ['-'] * 23;
 	for k in keys:
 		ak = hk = h(k)
 		j = 0
 
-		while(table_quartic[ak] != '-'):
+		while(table_quartic[ak] != '-' and '-' in table_quartic):
 			ak = (h(k) + j**4) % 23
 			j += 1
 
-		table_quartic[ak] = 'X'
+		table_quartic[ak] = k
 	return table_quartic
